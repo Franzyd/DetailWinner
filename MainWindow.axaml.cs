b71af8d4d6b534832d7a4002b47a07559a4203e3
Franzyd <firekfrank@gmail.com>
2025-05-04 20:58:53 +0200

    Add initial start window

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -13,16 +12,5 @@ public partial class MainWindow : Window
 
     private void Button_OnClick(object? sender, RoutedEventArgs e)
     {
-        if (double.TryParse(Celsius.Text, out double c))
-        {
-            var f = c * (9d / 5d) + 32;
-            Fahrenheit.Text = f.ToString("0.0");
-        }
-        else
-        {
-            Celsius.Text = "0";
-            Fahrenheit.Text = "0";
-        }
-        Debug.WriteLine($"Click! Celsius={Celsius.Text}");
     }
 }
